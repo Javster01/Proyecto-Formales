521c879412a84217c913d2264e9d69a1208def2e
Javster01 <javifelipeortiz@gmail.com>
2024-03-04 14:42:23 -0500

    Se agrego el numero de la linea del error

--- push_down_automata.py
+++ push_down_automata.py
@@ -10,7 +10,6 @@ compiler = Tk()
 compiler.title('Compilador Ruby/Julia')
 #se crea un arreglo de texto vacio para poner el path del archivo a abrir o a guardar
 file_path = ''
-
 class AutomataRuby:
     def __init__(self):
         self.estado_actual = 'inicial'
@@ -20,64 +19,49 @@ class AutomataRuby:
         self.symbol_table = {'global': {}}
         self.current_scope = 'global'
 
-    def estados(self, simbolo, stack, count):
+    def estados(self, simbolo, stack, count,pos):
         try:
             self.analisis+="q" + str(count) + " Stack:" + str(stack.items)+ "\n"
 
             if re.search('^print',simbolo) or re.search('^puts ',simbolo):
                 print('entra')
-                stack.apilar("salida")
+                stack.apilar("salida en linea "+str(pos))
                 self.states = count+1
                 simbolo = simbolo.replace('print','').replace('puts','')
-                self.estados(simbolo,stack,self.states)
+                self.estados(simbolo,stack,self.states,pos)
             elif re.search('^\w+ =', simbolo):
-                print('entra')
-                stack.apilar("variable")
+                stack.apilar("variable en linea "+str(pos))
                 self.states = count+1
-                self.estados(re.sub('^\w+ ','',simbolo),stack,self.states)
+                self.estados(re.sub('^\w+ ','',simbolo),stack,self.states,pos)
             elif re.search('^= .+', simbolo):
-                print('sale')
                 stack.desapilar()
             elif re.search('^(?!println\()\w+\(',simbolo):
-                print('entra')
-                stack.apilar("funcion")
+                stack.apilar("funcion en linea "+str(pos))
                 self.states = count+1
-                self.estados(re.sub('^(?!println\()\w+','',simbolo),stack,self.states)
+                self.estados(re.sub('^(?!println\()\w+','',simbolo),stack,self.states,pos)
             elif (re.search('^\(.+\)$', simbolo) or re.search('^".+"$', simbolo)or re.search('^ .+$', simbolo)) \
-                    and len(stack.items)!=0 and(stack.items[len(stack.items)-1]=='salida'
-                                                or stack.items[len(stack.items)-1]=='funcion'):
-                print('sale')
+                    and len(stack.items)!=0 and(re.findall(stack.items[len(stack.items)-1],'salida en linea \d+')!=None or re.findall(stack.items[len(stack.items)-1],'funcion en linea \d+')!=None):
                 stack.desapilar()
             elif re.search('^if\s+[^;\n]+;?', simbolo):
-                print('entra')
-                stack.apilar("condicional")
+                stack.apilar("condicional en linea "+str(pos))
             elif re.search('^elsif\s+[^;\n]+;?', simbolo):
-                print('sale')
                 stack.desapilar()
-                print('entra')
-                stack.apilar("condicional")
+                stack.apilar("condicional  en linea "+str(pos))
             elif re.search('^else\s+[^;\n]+;?', simbolo):
-                print('sale')
                 stack.desapilar()
-                print('entra')
-                stack.apilar("condicional")
+                stack.apilar("condicional en linea "+str(pos))
             elif re.search('^end', simbolo):
-                print('sale')
                 stack.desapilar()
             elif re.search('^\s*for\s+\w+\s+in\s+.+\s*;?\s*d?o?$', simbolo) :
-                print('entra')
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
             elif re.search('^\s*while\s+[^;\n]+;?\s*', simbolo):
-                print('entra')
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
             elif re.search('^\s*loop do;?\s*', simbolo):
-                print('entra')
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
             elif re.search('^\s*until\s+[^;\n]+ do;?\s*', simbolo):
-                print('entra')
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
         except:
-            stack.apilar("Error")
+            stack.apilar("Error en linea "+str(pos))
             pass
 
 
@@ -87,7 +71,7 @@ class AutomataRuby:
         countSupport = 0
         for i,simbolo in enumerate(codigo):
             simbolo = re.sub(r'^\s+','',simbolo)
-            self.estados(simbolo , pila, self.states)
+            self.estados(simbolo , pila, self.states,i)
 
         # Una vez analizado todo el código, verifica si la pila está vacía
         if pila.esta_vacia():
@@ -123,7 +107,6 @@ class AutomataRuby:
                         self.errors.append(f"Error: Número incorrecto de argumentos para la función '{function}'.")
     def report_errors(self):
         return "\n".join(self.errors)
-
 class AutomataJulia:
     def __init__(self):
         self.estado_actual = 'inicial'
@@ -133,52 +116,52 @@ class AutomataJulia:
         self.current_scope = 'global'
         self.symbol_table = {'global': {}}
 
-    def estados(self, simbolo, stack, count):
+    def estados(self, simbolo, stack, count,pos):
         self.analisis+="q" + str(count) + " Stack:" + str(stack.items)+ "\n"
         try:
             if re.search('^println|print',simbolo):
-                stack.apilar("salida")
+                stack.apilar("salida en linea "+str(pos))
                 self.states = count+1
-                self.estados(simbolo.replace('println',''),stack,self.states)
+                self.estados(simbolo.replace('println',''),stack,self.states,pos)
             elif re.search('^puts ".+"$', simbolo) or re.search('^gets.chomp+', simbolo) or re.search('^def +', simbolo) or re.search('^loop +', simbolo) or re.search('^until +', simbolo) :
-                stack.apilar('Ruby')
+                stack.apilar('Ruby en linea '+str(pos))
             elif re.search('^\w+ =', simbolo):
-                stack.apilar("variable")
+                stack.apilar("variable en linea "+str(pos))
                 self.states = count+1
-                self.estados(re.sub('^\w+ ','',simbolo),stack,self.states)
+                self.estados(re.sub('^\w+ ','',simbolo),stack,self.states,pos)
             elif re.search('^= .+', simbolo):
                 stack.desapilar()
             elif re.search('^(?!println\()\w+\(',simbolo):
-                stack.apilar("funcion")
+                stack.apilar("funcion en linea "+str(pos))
                 self.states = count+1
-                self.estados(re.sub('^(?!println\()\w+','',simbolo),stack,self.states)
-            elif re.search('^\(.+\)$', simbolo) and len(stack.items)!=0 and(stack.items[len(stack.items)-1]=='salida' or stack.items[len(stack.items)-1]=='funcion'):
+                self.estados(re.sub('^(?!println\()\w+','',simbolo),stack,self.states,pos)
+            elif re.search('^\(.+\)$', simbolo) and len(stack.items)!=0 and(re.findall(stack.items[len(stack.items)-1],'salida en linea \d+')!=None or re.findall(stack.items[len(stack.items)-1],'funcion en linea \d+')!=None):
                 stack.desapilar()
             elif re.search('^if\s+[^;\n]+;?', simbolo):
-                stack.apilar("condicional")
+                stack.apilar("condicional en linea "+str(pos))
             elif re.search('^elseif\s+[^;\n]+;?', simbolo):
                 stack.desapilar()
-                stack.apilar("condicional")
+                stack.apilar("condicional en linea "+str(pos))
             elif re.search('^else\s+[^;\n]+;?', simbolo):
                 stack.desapilar()
-                stack.apilar("condicional")
+                stack.apilar("condicional en linea "+str(pos))
             elif re.search('^end', simbolo):
                 stack.desapilar()
             elif re.search('^for\s+\w+\s+in\s+[^;\n]+;?\s*$', simbolo):
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
             elif re.search('^\s*while\s+[^;\n]+;?\s*', simbolo):
-                stack.apilar("Ciclo")
+                stack.apilar("Ciclo en linea "+str(pos))
         except:
-            stack.apilar("Error")
+            stack.apilar("Error en linea "+str(pos))
             pass
 
     def analizar(self, codigo):
         pila = Pila()
         count = 0
         countSupport = 0
-        for simbolo in codigo:
+        for i,simbolo in enumerate(codigo):
             simbolo = re.sub(r'^\s+','',simbolo)
-            self.estados(simbolo , pila, self.states)
+            self.estados(simbolo , pila, self.states,i)
 
         # Una vez analizado todo el código, verifica si la pila está vacía
         if pila.esta_vacia():
@@ -216,47 +199,37 @@ class AutomataJulia:
 def open_file():
     # guarda en path la ubicacion del archivo con el askopenfilename, en este caso solo busca archivoz .jl y .rb
     path = askopenfilename(filetypes=[('Julia/Ruby Files', '*.jl .rb')])
-    
-    
     with open(path, 'r') as file:
         # con el archivo detectado, hace el read para mandar el codigo a memoriam, lo pone en el espacio de texto de code_output y
         # llama la funcion detected_language para verificar el lenguaje
         code = file.read()
-        print(code.count)
         code_output.delete("1.0", END)
         detected_language = detect_language(code)
+
 
         # AQUI SE PONE EL OUTPUT DEL ANALISIS EN ESTE CASO EL TIPO DE LENGUAJE
 
 
 
         # AQUI SE PONE EL CODIGO EN EL ESPACIO DE TEXTO DE ARRIBA
-        
-
         editor.delete('1.0', END)
         editor.insert('1.0', code)
         set_file_path(path)
         codigo_julia = code
-
-        if(len(codigo_julia)>500):
-            code_output.insert("1.0","El codigo supera los 500 caracteres teniendo "+str(len(codigo_julia)))
-            return
-        if(len(codigo_julia)<50):
-            code_output.insert("1.0","El codigo es inferior a 50 caracteres teniendo "+str(len(codigo_julia)))
-            return
-        
         print("La cadena es de tamaño: ", len(codigo_julia))
         automata = AutomataJulia()
         codigo = re.split('\n|\r|\b" "', codigo_julia)
         code_output.insert("1.0", "El total de caracteres de la cadena es de: " + str(len(codigo_julia)) + "\n")
         analisis, pila, result = automata.analizar(codigo)
+        matches = [item for item in pila.items if re.search('Ruby en linea \d+', item)]
         if result != False:
             code_output.insert("1.0", "Código de Julia\n")
             code_output.insert("1.0", analisis)
             automata.analyze_julia_semantics(codigo_julia)
             errors = automata.report_errors()
             code_output.insert("1.0", errors)
-        elif "Ruby" in pila.items:
+        elif matches:
+            print('entraruby')
             automataRuby = AutomataRuby()
             analisis, pila, result = automataRuby.analizar(codigo)
             automataRuby.analyze_ruby_semantics(codigo_julia)
@@ -317,6 +290,7 @@ if __name__ == "__main__":
     # se agregan al menu desplegable los comando de open, save, save as, exit
     file_menu.add_command(label='Open', command=open_file)
     file_menu.add_command(label='Exit', command=exit)
+    
 
     # se pone despligue de tipo cascada con todos los items de file_menu y se añade a menu bar
     menu_bar.add_cascade(label='File', menu=file_menu)
